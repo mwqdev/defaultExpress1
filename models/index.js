1c66e46f2bbd4b2e1285658cb2ed59df67bb52c3
mwqdev <quarles.michael@gmail.com>
2017-06-30 15:34:55 -0400

    fixed models.index.js lines: 11, 13, 22

--- models/index.js
+++ models/index.js
@@ -8,9 +8,9 @@ const config    = require(__dirname + '/..\\config\\config.json')[env];
 const db        = {};
 
 if (config.use_env_variable) {
-  let sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  let sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
@@ -19,12 +19,12 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(function(file) {
-    let model = sequelize['import'](path.join(__dirname, file));
+    var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
+    if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
